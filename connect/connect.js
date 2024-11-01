2dc535bcbefc60e09904f7271388f8d151b70ed1
KumarRaj21 <konnakumarraja097@gmail.com>
2024-11-02 00:02:05 +0530

    made changes

--- connect/connect.js
+++ connect/connect.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = async ()=>{
     try {
         await mongoose
-        .connect("mongodb+srv://KonnaKumar:kumarraj@cluster0.vvfxp.mongodb.net/")
+        .connect("mongodb+srv://KonnaKumar:Konnakumar@cluster0.vvfxp.mongodb.net/")
         .then(()=>{
             console.log("connected")
         })
